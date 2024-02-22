9019487f14509423844494acc54b147026dfc779
Ashwani Kumar <itsashu268@gmail.com>
2024-02-22 17:48:51 +0530

    utill code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        massage="Something went wrong",
+        errors = [],
+        statck =""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.massage = massage
+        this.success = false;
+        this.errors =errors
+        
+        if (statck){
+            this.stack = statck
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+export {ApiError}
