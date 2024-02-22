799b5789dd325eb055db35e071e7706974c70566
Ashwani Kumar <itsashu268@gmail.com>
2024-02-22 23:00:57 +0530

    project setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECERET
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath, {resource}) return null
+        //upload the file on cloudinary
+      const response= await  cloudinary.uploader.upload(localFilePath, {
+            resource_type:"auto"
+        })
+        //File has been uploaded succesfully
+        console.log("File is uploaded on cloudinary", response.url);
+        return response
+
+    }catch(error){
+        fs.unlinkSync(localFilePath)//remove the locally saved tem files as the upload operation failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
